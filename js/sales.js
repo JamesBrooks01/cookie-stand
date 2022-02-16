caa81b7faccf09f248b64be7f12bc41450308d61
James Brooks <brooksjamesk01@gmail.com>
2022-02-15 17:02:10 -0800

    converted list into table

--- js/sales.js
+++ js/sales.js
@@ -8,6 +8,7 @@ function randomCustomerCount(min, max) {
 let timeSheet = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm',];
 let storeLocations = [];
 let totalSalesArray = [];
+let totalValues = [];
 
 function StoreLoc(storeName, minCust, maxCust, avgCookie) {
   this.storeName = storeName;
@@ -44,6 +45,19 @@ StoreLoc.prototype.render = function () {
   }
 };
 
+function timeRow() {
+  timeSheet.push('Daily Location Total');
+  let timeRow = document.createElement('tr');
+  salesTable.appendChild(timeRow);
+  let rowSpacer = document.createElement('th');
+  timeRow.appendChild(rowSpacer);
+  for (let i = 0; i < timeSheet.length; i++) {
+    let curTime = document.createElement('th');
+    curTime.textContent = timeSheet[i];
+    timeRow.appendChild(curTime);
+  }
+
+}
 
 function sum(a, b) {
   let answer1 = a + b;
@@ -67,7 +81,51 @@ function sumAnyArray(a) {
   //console.table(array6);
   return array6;
 }
+
+function sumTotalArray(a) {
+  let answerSumArr = [];
+  let b = 0;
+  for (let i = 0; i <= totalSalesArray.length - 3; i++) {
+    let sumOne = sum(totalSalesArray[b][a], totalSalesArray[b + 1][a])[0];
+    answerSumArr.splice(0, 1, sumOne);
+    let sumTwo = sum(answerSumArr[b], totalSalesArray[b + 2][a])[0];
+    answerSumArr.push(sumTwo);
+    b++;
+  }
+  let lastValue = answerSumArr[answerSumArr.length - 1];
+  let array6 = [lastValue, `The numbers ${a.cookieSalesArray} have a product of ${lastValue}.`];
+  //console.log(totalValues);
+  totalValues.push(lastValue);
+  return array6;
+}
+
+function sumTotalCalc() {
+  for(let i =0; i < 15; i++){
+    sumTotalArray(i);
+  }
+}
+
+function totalRow(){
+  let totalRow = document.createElement('tr');
+  salesTable.appendChild(totalRow);
+  let totalElem = document.createElement('th');
+  totalElem.textContent = 'Totals';
+  totalRow.appendChild(totalElem);
+  for (let i = 0; i < totalValues.length; i++) {
+    let totalData = document.createElement('th');
+    totalData.textContent = `${totalValues[i]} cookies`;
+    totalRow.appendChild(totalData);
+  }
+}
+
+new StoreLoc('Seattle', 23, 65, 6.3);
+new StoreLoc('Tokyo', 3, 24, 1.2);
+new StoreLoc('Dubai', 11, 38, 3.7);
+new StoreLoc('Paris', 20, 38, 2.3);
+new StoreLoc('Lima', 2, 16, 4.6);
+
 function renderAllCities() {
+  timeRow();
   for (let i = 0; i < storeLocations.length; i++) {
     let currentCity = storeLocations[i];
     currentCity.randomCust();
@@ -75,5 +133,7 @@ function renderAllCities() {
     sumAnyArray(storeLocations[i]);
     currentCity.render();
   }
+  sumTotalCalc();
+  totalRow();
 }
 renderAllCities();
